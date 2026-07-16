c9b98f1a3132fb7dc4d6f40adba65507b861de6f
yuexps <2899659758@qq.com>
2026-07-16 21:39:47 +0800

    chore: clean up redundant server code and build cache

--- server/mod/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import searchx as searchx
-from . import search_util as search_util
-search_all = search_util.search_song_best
--- server/mod/searchx/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import qq as qq
-from . import netease as netease
-from . import kugou as kugou
-
